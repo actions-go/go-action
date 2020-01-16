1070152ca06f18770fb6c3d126e13b460da761e6
Thibault Jamet <tjamet@users.noreply.github.com>
2020-01-16 21:51:29 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+var now = func() time.Time {
+	return time.Now()
+}
+
+func runMain(w io.Writer) {
+	sleep := os.Getenv("INPUT_MILLISECONDS")
+	fmt.Fprintf(w, "::debug::Waiting %s milliseconds\n", sleep)
+	fmt.Fprintln(w, "::debug::"+now().String())
+	delay, err := strconv.Atoi(sleep)
+	if err != nil {
+		fmt.Fprintln(w, "::error::"+err.Error())
+		return
+	}
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	fmt.Fprintln(w, "::debug::"+now().String())
+	fmt.Fprintln(w, "::set-output name=time::"+now().String())
+}
+
+func main() {
+	runMain(os.Stdout)
+}
