3e449e7f51865281f858e14c69ed39500aacdf21
Thibault Jamet <tjamet@users.noreply.github.com>
2023-10-09 22:34:22 +0200

    try to use with syntax

--- main.go
+++ main.go
@@ -2,34 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"time"
 
 	"github.com/actions-go/toolkit/core"
 )
 
-var now = func() time.Time {
-	return time.Now()
-}
+var (
+	exit = os.Exit
+	now  = func() time.Time {
+		return time.Now()
+	}
+)
 
-func runMain() {
+func main() {
 	sleep, ok := core.GetInput("myInput")
 	if !ok {
 		core.Error("Unable to find required input myInput")
+		exit(1)
 	}
-	fmt.Println(sleep)
 	core.Debug(fmt.Sprintf("Waiting %s milliseconds", sleep))
 	core.Debug(now().String())
 	delay, err := strconv.Atoi(sleep)
 	if err != nil {
 		core.Error(err.Error())
-		return
+		exit(1)
 	}
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 	core.Debug(now().String())
 	core.SetOutput("time", now().String())
-}
-
-func main() {
-	runMain()
 }
--- main_test.go
+++ main_test.go
@@ -3,39 +3,72 @@ package main
 import (
 	"bytes"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/actions-go/toolkit/core"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
-	errorOutput = `::debug::Waiting  milliseconds
-::debug::2020-01-10 20:10:20.000000001 +0000 UTC
-::error::strconv.Atoi: parsing "": invalid syntax
+	errorOutput = `::debug::Did not find the input using plain gha input format%2C trying the actions-go one
+::error::Unable to find required input myInput
 `
 	successOutput = `::debug::Waiting 10 milliseconds
 ::debug::2020-01-10 20:10:20.000000001 +0000 UTC
 ::debug::2020-01-10 20:10:20.000000001 +0000 UTC
-::set-output name=time::2020-01-10 20:10:20.000000001 +0000 UTC
 `
 )
 
 func TestRunMain(t *testing.T) {
-	w := bytes.NewBuffer(nil)
-	now = func() time.Time {
-		return time.Date(2020, 01, 10, 20, 10, 20, 1, time.UTC)
-	}
-	core.SetStdout(w)
-	runMain()
+	t.Run("When the input is not available", func(t *testing.T) {
+		exitMagic := "test-run-main"
+		defer func() {
+			if r := recover(); r != nil {
+				assert.Equal(t, exitMagic, r)
+			}
+		}()
+		t.Cleanup(func() {
+			exit = os.Exit
+		})
+		exit = func(code int) {
+			assert.Equal(t, 1, code)
+			panic(exitMagic)
+		}
 
-	assert.Equal(t, errorOutput, w.String())
-	os.Setenv("INPUT_MYINPUT", "10")
+		w := bytes.NewBuffer(nil)
+		core.SetStdout(w)
+		main()
 
-	w = bytes.NewBuffer(nil)
-	core.SetStdout(w)
-	runMain()
+		assert.Equal(t, errorOutput, strings.Replace(w.String(), "%3A", ":", -1))
 
-	assert.Equal(t, successOutput, w.String())
+	})
+
+	t.Run("When the input is available", func(t *testing.T) {
+		fd, err := os.CreateTemp("", "githuboutput")
+		require.NoError(t, err)
+
+		outputPath := fd.Name()
+		os.Setenv("GITHUB_OUTPUT", outputPath)
+		t.Cleanup(func() {
+			fd.Close()
+			os.Remove(outputPath)
+
+		})
+		os.Setenv("INPUT_MYINPUT", "10")
+		exit = func(code int) {
+			assert.Equal(t, 0, code)
+		}
+		now = func() time.Time {
+			return time.Date(2020, 01, 10, 20, 10, 20, 1, time.UTC)
+		}
+
+		w := bytes.NewBuffer(nil)
+		core.SetStdout(w)
+		main()
+
+		assert.Equal(t, successOutput, strings.Replace(w.String(), "%3A", ":", -1))
+	})
 }
